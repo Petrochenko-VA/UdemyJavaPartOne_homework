0fa716b1d1f8bf1424c0ff8fb885cb8d0d0a6efb
VladimirPetrochenko <uzbek150885@gmail.com>
2023-10-29 19:15:08 +0300

    17

---
+++ src/Homework/Lesson17/String_Bulder.java
@@ -0,0 +1,32 @@
+package Homework.Lesson17;
+
+public class String_Bulder {
+
+    static boolean ravenstvo(StringBuilder sb1, StringBuilder sb2){
+
+        boolean resultBoolean =true ;
+        if(sb1.length() != sb2.length()){
+            resultBoolean = false;
+        }else {
+            for (int i = 0; i < sb1.length(); i++) {
+                if (sb1.charAt(i) != sb2.charAt(i)) {
+                    if (resultBoolean = false) {
+                        break;
+                    }
+                    resultBoolean = false;
+
+                }
+            }
+        }
+
+
+        return resultBoolean;
+    }
+
+    public static void main(String[] args) {
+        StringBuilder sb1 = new StringBuilder("priveT");
+        StringBuilder sb2 = new StringBuilder("privet");
+        System.out.println( ravenstvo(sb1,sb2));
+
+    }
+}
